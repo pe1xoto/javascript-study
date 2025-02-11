96f59e48e1c3e10f6a4b29d034a5fcd0b8a2066b
pe1xoto <157078050+pe1xoto@users.noreply.github.com>
2025-02-11 20:55:03 -0300

    Add files via upload

---
+++ peixotoscript.js
@@ -0,0 +1,7 @@
+//alert('Hello, welcome to Peixoto s learnings!')
+
+/*
+    Select an element in the DOM  
+    update the value of this element with a string
+*/
+document.getElementById('name').value = 'lol'
